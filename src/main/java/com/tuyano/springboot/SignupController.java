962eb5bc52aa0eb6295ab2ff3501b69a0929ef5e
Ito <dolkas12@gmail.com>
2019-07-09 16:08:32 +0900

    <20190709>

--- src/main/java/com/tuyano/springboot/SignupController.java
+++ src/main/java/com/tuyano/springboot/SignupController.java
@@ -3,19 +3,30 @@ package com.tuyano.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 
 @Controller
 public class SignupController {
+	
 @GetMapping("/login")
 public String getLogin(Model model) {
 	return ("login");
 }
 @GetMapping("/signup")
-public String getSignUp(Model model) {
+public String getSignUp(@ModelAttribute SignupForm form , Model model) {
 	return"signup";
 }
-public String postSginUp(Model model) {
-	return "login";
+@PostMapping("/signup")
+public String postSginUp(@ModelAttribute @Validated SignupForm form, 
+		BindingResult bindingResult ,Model model) {
+	if(bindingResult.hasErrors()) {
+		return getSignUp(form,model);
+	}
+	System.out.println(form);
+	return "redirect:/login";
 }
 }
--- src/main/java/com/tuyano/springboot/SignupForm.java
+++ src/main/java/com/tuyano/springboot/SignupForm.java
@@ -1,5 +1,21 @@
 package com.tuyano.springboot;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
 public class SignupForm {
-
+@NotBlank
+@Email
+	private String userId;
+@NotBlank
+@Length(min=4,max=100)
+@Pattern(regexp="^[a-zA-Z0-9]+$")
+	private String password;
+@NotBlank
+	private String userName;
 }
