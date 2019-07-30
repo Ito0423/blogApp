b4e7bd760ae71ce6b2ee379d19b6663b607d368b
Ito <dolkas12@gmail.com>
2019-07-30 11:04:14 +0900

    <2019 0730>

--- src/main/java/com/tuyano/springboot/repositories/AccountRepository.java
+++ src/main/java/com/tuyano/springboot/repositories/AccountRepository.java
@@ -3,9 +3,10 @@ package com.tuyano.springboot.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tuyano.springboot.data.UserData;
-public interface AccountRepository extends JpaRepository<UserData, String> {
-    public UserData findByUsername(String username);
-    public Optional<UserData> findById(Long name);
+@Repository
+public interface AccountRepository extends JpaRepository<UserData, Integer> {
+	public UserData findById(String loginId);
 }
